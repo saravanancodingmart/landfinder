fe95c24c27a1ecc32be0e35ee118bc14ceda273b
Saravanan codingmart <saravanan@landmarkwebteam.com>
2020-01-05 23:42:21 +0530

    modified index.html

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ var express = require('express')
 const request = require('request');
 var app = express()
 
-const options = {
-    url: 'https://atlas.mapmyindia.com/api/places/nearby/json?keywords=coffee&refLocation=12.927162532132044,77.69798034515283',  
-    headers: {
-      "Authorization": "bearer 79c9f6ed-5b05-40e2-a815-52bf2ccd4eb2",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-      "Access-Control-Allow-Methods": "*"
-    }
-};
-
 app.get('/', (req, res) => {
+
+    const options = {
+        url: 'https://atlas.mapmyindia.com/api/places/nearby/json?keywords=coffee&refLocation=12.927162532132044,77.69798034515283',  
+        headers: {
+          "Authorization": "bearer 79c9f6ed-5b05-40e2-a815-52bf2ccd4eb2",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Methods": "*"
+        }
+    };
+
     function callback(error, response, body) {
         if (!error && response.statusCode == 200) {
           const info = JSON.parse(body);
           res.send(info);
         }
-      }     
+    }
+
     request(options, callback);
 })
 
