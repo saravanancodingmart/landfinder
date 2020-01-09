0b317a3299e3b4f7b232b30470654bf33b23f057
Saravanan codingmart <saravanan@landmarkwebteam.com>
2020-01-09 18:35:33 +0530

    Fixed The issues regarding post call

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ var app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  const { query, current_lat, current_lng} = req.param 
+  const { query, current_lat, current_lng } = req.query;
   const optionsPost = {
     url:'https://outpost.mapmyindia.com/api/security/oauth/token', 
-    data: {
+    headers: {'content-type' : 'application/x-www-form-urlencoded'},
+    form: {
       grant_type: 'client_credentials',
       client_id: 'R1COQ57r0_UddN-80_NLFt89BHEFeX7cTXmwVG2tdiT06OyXbOqgukHFpt3xfxOm-tpo5Y6JKN431cJTLTE6Zw==',
       client_secret: '9K_q_9Q2GHMAbyNdcof10gRcszQGZcpOfzv62cEwJ_Pqpd-_GsP_53Qy-hi6k1x-3RJohmyoMwOOZ0mR9Hh6KYmU695pGdj8'
@@ -17,9 +18,9 @@ app.get('/', (req, res) => {
     if (!error && response.statusCode == 200) {
       const { access_token, token_type } = JSON.parse(body);
       const optionsGet = {
-          url: 'https://atlas.mapmyindia.com/api/places/nearby/json?keywords=${query}&refLocation=${current_lat},${current_lng}',         
+          url: "https://atlas.mapmyindia.com/api/places/nearby/json?keywords="+query+"&refLocation="+current_lat+","+current_lng,         
           headers: {
-            "Authorization": "${token_type} ${access_token}",
+            "Authorization": token_type+" "+access_token,
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": true,
             "Access-Control-Allow-Methods": "*"
@@ -28,15 +29,15 @@ app.get('/', (req, res) => {
       request(optionsGet,  function getCallback(error, response, body) {
         if (!error && response.statusCode == 200) {
           const info = JSON.parse(body);
-          response.send(info);
+          res.send(info)
         }
         else {
-          return console.error(error);
+          console.log(error);
         }
       });
     }
     else {
-      return console.error(error);
+      console.log(error);
     }
   });
 })
